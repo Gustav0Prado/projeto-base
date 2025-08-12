8fdc6c6c3a81181e739de73848e4e80907835237
gustavops <guprado.silva@gmail.com>
2025-08-12 12:04:20 -0300

    Issue #1: Footer

---
+++ src/components/sections/layout/Footer.vue
@@ -0,0 +1,172 @@
+<template>
+    <footer class="flex flex-col flex-wrap gap-20 py-40 items-left">
+        <div class="flex flex-row flex-wrap gap-35">
+            <div v-for="(column, index) in links" :key="index" class="flex flex-col gap-2 items-start">
+                <a v-for="(link, subindex) in column" :key="subindex" :href="link.link" class="hover:underline">
+                    {{ link.name }} </a>
+            </div>
+        </div>
+        <div class="flex flex-row relative">
+            <small class="font-thin text-gray-600">
+                © 2016 Free HTML5. All Rights Reserved. <br>
+                Designed by
+                <a href="FreeHTML5.co" class="text-orange-600"> FreeHTML5.co </a>
+                Demo Images:
+                <a href="Unsplash.com" class="text-orange-600"> Unsplash </a>
+            </small>
+            <div class="flex flex-row gap-4 absolute right-0 inset-y-0">
+                <a href="https://twitter.com" target="_blank">
+                    <img src="../../../assets/twitter.png" alt="Twitter" width="24" height="24">
+                </a>
+                <a href="https://facebook.com" target="_blank">
+                    <img src="../../../assets/facebook.png" alt="facebook" width="24" height="24">
+                </a>
+                <a href="https://linkedin.com" target="_blank">
+                    <img src="../../../assets/linkedin.png" alt="linkedin" width="24" height="24">
+                </a>
+                <a href="https://twitter.com" target="_blank">
+                    <img src="../../../assets/twitter.png" alt="Twitter" width="24" height="24">
+                </a>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script setup lang="ts">
+const links = [
+    [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Help",
+            link: "/"
+        },
+        {
+            name: "Contact",
+            link: "/"
+        },
+        {
+            name: "Terms",
+            link: "/"
+        },
+        {
+            name: "Meetups",
+            link: "/"
+        },
+    ],
+    [
+        {
+            name: "Creative Design",
+            link: "/about"
+        },
+        {
+            name: "Branding",
+            link: "/"
+        },
+        {
+            name: "Illustration",
+            link: "/"
+        },
+        {
+            name: "Subscribe",
+            link: "/"
+        },
+        {
+            name: "Monthly Event",
+            link: "/"
+        },
+    ],
+    [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Help",
+            link: "/"
+        },
+        {
+            name: "Contact",
+            link: "/"
+        },
+        {
+            name: "Terms",
+            link: "/"
+        },
+        {
+            name: "Meetups",
+            link: "/"
+        },
+    ],
+    [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Help",
+            link: "/"
+        },
+        {
+            name: "Contact",
+            link: "/"
+        },
+        {
+            name: "Terms",
+            link: "/"
+        },
+        {
+            name: "Meetups",
+            link: "/"
+        },
+    ],
+    [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Help",
+            link: "/"
+        },
+        {
+            name: "Contact",
+            link: "/"
+        },
+        {
+            name: "Terms",
+            link: "/"
+        },
+        {
+            name: "Meetups",
+            link: "/"
+        },
+    ],
+    [
+        {
+            name: "About",
+            link: "/about"
+        },
+        {
+            name: "Help",
+            link: "/"
+        },
+        {
+            name: "Contact",
+            link: "/"
+        },
+        {
+            name: "Terms",
+            link: "/"
+        },
+        {
+            name: "Meetups",
+            link: "/"
+        },
+    ]
+]
+</script>
+
+<style scoped></style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,44 @@
-import { VitePWA } from 'vite-plugin-pwa';
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    injectRegister: false,
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
 
-    pwaAssets: {
-      disabled: false,
-      config: true,
-    },
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
 
-    manifest: {
-      name: 'my-vue-app',
-      short_name: 'my-vue-app',
-      description: 'my-vue-app',
-      theme_color: '#ffffff',
-    },
+      manifest: {
+        name: "my-vue-app",
+        short_name: "my-vue-app",
+        description: "my-vue-app",
+        theme_color: "#ffffff",
+      },
 
-    workbox: {
-      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
-      cleanupOutdatedCaches: true,
-      clientsClaim: true,
-    },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
 
-    devOptions: {
-      enabled: false,
-      navigateFallback: 'index.html',
-      suppressWarnings: true,
-      type: 'module',
-    },
-  })],
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
   preview: {
-    allowedHosts: ['souufpr.teste.ufpr.br','sistemas.teste.ufpr.br']
-  }
-})
+    allowedHosts: ["souufpr.teste.ufpr.br", "sistemas.teste.ufpr.br"],
+  },
+});
